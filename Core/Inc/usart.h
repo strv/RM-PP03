2bd95511cc29beea7fd7c0416c9611aab57a3061
strv <strv.analoger@gmail.com>
2023-12-09 23:35:54 +0900

    move def position

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -36,18 +36,18 @@ extern "C" {
 /* USER CODE END Includes */
 
 /* USER CODE BEGIN Private defines */
-void usart_init();
-void usart1_rx_cb();
-void usart1_tx_dma_cb();
-int usart1_puts(const uint8_t const * pbuf, const int length);
-int usart1_getc();
+
 /* USER CODE END Private defines */
 
 void MX_USART1_UART_Init(void);
 void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
-
+void usart_init();
+void usart1_rx_cb();
+void usart1_tx_dma_cb();
+int usart1_puts(const uint8_t const * pbuf, const int length);
+int usart1_getc();
 /* USER CODE END Prototypes */
 
 #ifdef __cplusplus
